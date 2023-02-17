ea4bcc3dcd9eb4ab8bbe08005964bd2bdcc50efd
JcousoG <afdt@alupostal4.local>
2023-02-17 20:31:55 +0100

    cambiei unha noticia

--- src/vistas/vistaprincipal.jsx
+++ src/vistas/vistaprincipal.jsx
@@ -4,7 +4,7 @@ function Vistaprincipal() {
        
     return(
         <>
-        <Novas titulo="Este é o título da primeira nova" contido="Este é o contido da primeira nova principal" />
+        <Novas titulo="«¿Dónde está Skadi?» Una familia de Teo denuncia el robo de su perro y ofrece 300 euros por recuperarlo" contido="Skadi tiene seis meses, una mancha blanca inconfundible en el pecho que destaca sobremanera sobre su pelaje negro y las puntas de las cuatro patas también blancas como la nieve. Este perro es, además, la alegría de su familia, que desde el pasado día 5 lo busca sin descanso tras desaparecer de su domicilio: una vivienda de Penelas, en Teo, con finca vallada en la que vivía Skadi hasta que desapareció sin dejar rastro. No es un perro de raza ni tiene pedigrí, ya que es una mezcla de border collie y pastor alemán, un mestizaje apenas conocido como tal pero que tiene nombre: shollie." />
         <Novas titulo="Este é o título da segunda nova" contido="Este é o contido da segunda nova principal" />
         <Novas titulo="Este é o título da terceira nova" contido="Este é o contido da terceira nova principal" />
         </>
